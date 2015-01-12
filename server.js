ccbc4586d191185a828c28f133cfbc061c77c07e
Ubuntu <cory@Android-Dev.coryandroiddev.d2.internal.cloudapp.net>
2015-01-12 21:46:26 +0000

    Fixed promise code and other stuff.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('./http.js');
 var config = require('./config.json');
+var promise = require('promise');
 
 process.on('uncaughtException', function (err) {
   console.log('Caught exception: ' + err);
@@ -11,37 +12,51 @@ http.get(config.sourceDb + '/_all_dbs').then(function(res) {
 
       console.log('Databases set for replication: ' + dbList.join(', '));
 
-      for (var db in dbList) {
-        // exclude _ and test databases
-        if (dbList[db].substr(0, 1) == '_' || dbList[db].substr(0, 10) == 'test_suite') {continue;}
+      var promiseList = [];
 
-        var loginUrl = buildLoginUrl(config.sourceDb, config.sourceDbUserName, config.sourceDbPassword);
-        var targetLoginUrl = buildLoginUrl(config.targetDb, config.targetDbUserName, config.targetDbPassword);
-        http.post(loginUrl + '/_replicate', {
-          'source': dbList[db],
-          'target': targetLoginUrl + '/' + dbList[db]
-        }).then(function(res) {
-          if (res.ok) {
-            if (config.logLevel == 'debug') {
-              console.log(dbList[db] + ': ' + res);
-            } else {
-              var logRes = {
-                database: dbList[db],
-                changes: (res.no_changes == undefined),
-                docs_written: res.history[0].docs_written,
-                doc_write_failures: res.history[0].doc_write_failures
-              };
-              console.log('Database replicated successfully: ' + JSON.stringify(logRes));
-            }
-          } else {
-            console.log(res);
-          }
-        }, function(err) { console.log(err); });
+      for (var db in dbList) {
+        var dbName = dbList[db];
+        // exclude _ and test databases
+        if (dbName.substr(0, 1) == '_' || dbName.substr(0, 10) == 'test_suite') {continue;}
+
+        promiseList.push(replicateDb(dbName));
       }
+
+      return promises.all(promiseList);
     }, function(err) { console.log(err); }
 );
 
 function buildLoginUrl(base, user, pass) {
   var urlParts = base.split('//');
   return urlParts[0] + '//' + user + ':' + pass + '@' + urlParts[1];
+}
+
+function replicateDb(dbName) {
+  var loginUrl = buildLoginUrl(config.sourceDb, config.sourceDbUserName, config.sourceDbPassword);
+  var targetLoginUrl = buildLoginUrl(config.targetDb, config.targetDbUserName, config.targetDbPassword);
+  return http.post(loginUrl + '/_replicate', {
+    'source': dbName,
+    'target': targetLoginUrl + '/' + dbName,
+    'create_target': true
+  }).then(function(res) {
+       handleReplicateResponse(dbName, res);
+  }, function(err) { console.log(err); });
+}
+
+function handleReplicateResponse(dbName, res) {
+  if (res.ok) {
+    if (config.logLevel == 'debug') {
+      console.log(dbName + ': ' + res);
+    } else {
+      var logRes = {
+        database: dbName,
+        changes: (res.no_changes == undefined),
+        docs_written: res.history[0].docs_written,
+        doc_write_failures: res.history[0].doc_write_failures
+      };
+      console.log('Database replicated successfully: ' + JSON.stringify(logRes));
+    }
+  } else {
+    console.log(res);
+  }
 }
